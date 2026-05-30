10de2804da6fc2244d21b707e964044c2749e606
Rafael Sadowski <rafael@sizeofvoid.org>
2026-05-30 17:42:20 +0200

    Import regress from base

---
+++ regress/patterns/patterns-tester.d
@@ -0,0 +1,27 @@
+patterns-tester: \
+  /home/rsadowski/src/rsadowski.gothub.org/httpd/regress/patterns/../..//patterns.c \
+  /usr/include/sys/types.h /usr/include/sys/cdefs.h \
+  /usr/include/machine/cdefs.h /usr/include/sys/endian.h \
+  /usr/include/sys/_endian.h /usr/include/sys/_types.h \
+  /usr/include/machine/_types.h /usr/include/machine/endian.h \
+  /usr/include/ctype.h /usr/include/errno.h /usr/include/sys/errno.h \
+  /usr/include/stddef.h /usr/include/sys/_null.h /usr/include/stdlib.h \
+  /usr/include/string.h /usr/include/strings.h \
+  /home/rsadowski/src/rsadowski.gothub.org/httpd/regress/patterns/../../patterns.h
+/usr/include/sys/types.h:
+/usr/include/sys/cdefs.h:
+/usr/include/machine/cdefs.h:
+/usr/include/sys/endian.h:
+/usr/include/sys/_endian.h:
+/usr/include/sys/_types.h:
+/usr/include/machine/_types.h:
+/usr/include/machine/endian.h:
+/usr/include/ctype.h:
+/usr/include/errno.h:
+/usr/include/sys/errno.h:
+/usr/include/stddef.h:
+/usr/include/sys/_null.h:
+/usr/include/stdlib.h:
+/usr/include/string.h:
+/usr/include/strings.h:
+/home/rsadowski/src/rsadowski.gothub.org/httpd/regress/patterns/../../patterns.h:
